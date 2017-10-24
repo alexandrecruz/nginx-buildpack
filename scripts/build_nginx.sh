c7ae03226edbe5777d93cca71456ff5da991f619
Alexandre Cruz <alexandrecruz91@gmail.com>
2017-10-23 22:24:02 -0300

    Update build_nginx.sh

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -9,13 +9,13 @@
 # Once the dyno has is 'up' you can open your browser and navigate
 # this dyno's directory structure to download the nginx binary.
 
-NGINX_VERSION=${NGINX_VERSION-1.9.4}
-PCRE_VERSION=${PCRE_VERSION-8.21}
+NGINX_VERSION=${NGINX_VERSION-1.12.1}
+PCRE_VERSION=${PCRE_VERSION-8.41}
 HEADERS_MORE_VERSION=${HEADERS_MORE_VERSION-0.23}
-OPEN_SSL_VERSION=${OPEN_SSL_VERSION-1.0.1p}
+OPEN_SSL_VERSION=${OPEN_SSL_VERSION-1.1.0f}
 
 nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
-pcre_tarball_url=http://garr.dl.sourceforge.net/project/pcre/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.bz2
+pcre_tarball_url=https://sourceforge.net/projects/pcre/files/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.bz2
 headers_more_nginx_module_url=https://github.com/agentzh/headers-more-nginx-module/archive/v${HEADERS_MORE_VERSION}.tar.gz
 open_ssl_url=https://www.openssl.org/source/openssl-${OPEN_SSL_VERSION}.tar.gz
 
@@ -46,7 +46,7 @@ echo "Downloading $open_ssl_url"
 		--with-pcre=pcre-${PCRE_VERSION} \
 		--prefix=/tmp/nginx \
 		--add-module=${temp_dir}/nginx-${NGINX_VERSION}/headers-more-nginx-module-${HEADERS_MORE_VERSION} \
-    --with-http_ssl_module --with-openssl=${temp_dir}/nginx-${NGINX_VERSION}/openssl-${OPEN_SSL_VERSION}
+    		--with-http_ssl_module --with-openssl=${temp_dir}/nginx-${NGINX_VERSION}/openssl-${OPEN_SSL_VERSION}
 	make install
 )
 
